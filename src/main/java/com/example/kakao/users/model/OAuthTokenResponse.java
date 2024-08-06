ab934968f46ad9ff87077d6083388d3d7c7ee0a2
c0smosaur <sjkang539@gmail.com>
2024-08-06 17:30:21 +0900

    ADD: added OAuthTokenResponse
    카카오 OAuth2 토큰 응답 객체

---
+++ src/main/java/com/example/kakao/users/model/OAuthTokenResponse.java
@@ -0,0 +1,15 @@
+package com.example.kakao.users.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OAuthTokenResponse {
+    private String tokenType;
+    private String accessToken;
+    private Integer expiresIn;
+    private String refreshToken;
+    private Integer refreshTokenExpiresIn;
+    private String scope;
+}
